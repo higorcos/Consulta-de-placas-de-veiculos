aef3d9721805741254a9954d5c3c61113739c097
higor costa <higorcosta2001@outlook.com>
2021-03-29 22:11:29 -0300

    .

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const express = require('express'); //importando biblioteca 
+/* const express = require('express'); //importando biblioteca 
 const app = express(); // cria uma instacia do express
 const bodyParser = require('body-parser') // bibilioteca para trabalhorar com formulários 
 const database = require('./database/database')//conexão com banco de dados
+ */
+
+import express from 'express'
+const app = express(); // cria uma instacia do express
+import bodyParser from 'body-parser'
+import database from "./database/database.js"
 
 //Tabela 
-const Plates = require('./plates/plates')
-//Dice plates 
-//const dicePlates = require('./plates/dice.plates')
-const dicePlates = require('./plates/platesData')
+//const Plates = require('./plates/plates')
+import Plates from "./plates/plates.js";
+//platesData   placas 
+import platesData from './plates/platesData.js'
+//const platesData = require('./plates/platesData')
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -25,7 +32,7 @@ database
 
 app.get('/', (req, res) => {
     //res.send('Page')
-    Plates.bulkCreate(dicePlates).then(() => {
+    Plates.bulkCreate(platesData.information).then(() => {
         // vai salvar no bando de dados os dados do variavel que esta sendo importada do arquivo dice.plates.js
         // esse arquivo possui placas gerados aleatoriamente com localidades aleatorias
         res.send('Era só teeeeste um booyyy')
@@ -33,9 +40,30 @@ app.get('/', (req, res) => {
     //res.send('.....')
 })
 
-app.get('/kk', (req, res) => {
-    res.send('//////////////////////')
+app.get('/teste/:plate', (req, res) => {
+    let plateSearch = req.params.plate;
+
+    Plates.findOne({
+        where: {plates: plateSearch}
+        //module exporte (variavel tá junta com outra no array) 
+    }).then((plate)=>{
+        if(plate == undefined){
+
+            res.send('Não tá no banco de dados ')
+        }else{
+            res.send('erro')
+        }
+       /*  Plates.create({
+            plates: plateSearch,
+            state: "teste"
+        }).then(() => {
+            
+        }) */
+        
+    })
 })
+
+
 app.listen(8080, () => {
     console.log('///////////////Servidor online ')
 })
